2ed394e80d6390f483bdcddf9e1cd33db461e907
Tim Miller <tim@rocket5studios.com>
2025-02-22 02:28:16 -0500

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Cinema Club Voting</h1>
+    </div>
+  );
+}
+
+export default App;
